0514706f96ad03f1457b98f92d6f6cf315aa961b
tamabba2979beai25-glitch <tamanna2979.beai25@chitkara.edu.in>
2025-12-17 15:28:10 +0530

    Program4

---
+++ sort_list.py
@@ -0,0 +1,6 @@
+def sort_list(numbers):
+    """Sort a list of numbers in ascending order."""
+    return sorted(numbers)  
+# Example usage:
+my_numbers = [5, 2, 9, 1, 5, 6]
+sorted_numbers = sort_list(my_numbers)
